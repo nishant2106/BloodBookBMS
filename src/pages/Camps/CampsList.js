ed7e4f52da379e17e5615d07174d1ba643028c04
Nishant Pandey <59759332+nishant2106@users.noreply.github.com>
2022-10-05 18:42:13 +0530

    warning

--- src/pages/Camps/CampsList.js
+++ src/pages/Camps/CampsList.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Button, Table } from "reactstrap";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Axios from "axios";
 
 class CampsList extends Component {
   constructor(props) {
